cb5993a05032fd81f67a158392900f49ae3d384c
connorkindley <90717044+connorkindley@users.noreply.github.com>
2022-01-06 01:07:13 -0500

    Updated main.py
    A friend provided me with wonderful suggestions to help make my program much more clear. More of the routines are kept as functions to help with readability.  I also changed how I was importing the functions and settings in order to provide a clear location for the functions and settings used.

--- Main.py
+++ Main.py
@@ -1,101 +1,67 @@
-from utils.settings import *
-from utils.functions import *
+from utils import settings
+from utils import functions
+import pygame
 
-SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Connect Four")
+pygame.init()
 
-run = True
-clock = pygame.time.Clock()  
-turn = 0
-# draw the default screen
-draw(SCREEN)    #keeping this outside the loop makes it so when you draw on it things dont disappear
-board = empty_board()
-game_on = True
-deciding = True
+def main():
+    SCREEN = pygame.display.set_mode((settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT))
+    pygame.display.set_caption("Connect Four")
 
+    # Setting up the default screen
+    functions.draw_board(SCREEN)
+    board = functions.initialize_board()
 
-while game_on:
-    clock.tick(FPS)
-#    game = Game(SCREEN)
+    # Game Play Initializations
+    game_on = True
+    allowed = True  # used for placing pieces
+    clock = pygame.time.Clock()  
+    turn = 0
 
-    for event in pygame.event.get():        
-        #so we can press X to quit the game
-        if event.type == pygame.QUIT:
-            game_on = False 
+    while game_on:
 
-        #This allows the player to see who's turn it is. Outside the click loop so you know the turn before clicking.
-        player_turn(SCREEN,turn)
+        clock.tick(settings.FPS)
 
-        #what happens when we click on the board
-        if event.type == pygame.MOUSEBUTTONDOWN:    
-            
-            pos = pygame.mouse.get_pos()
-            row, col = get_row_col_from_mouse(pos)
+        for event in pygame.event.get():  
 
-            if turn % 2 == 0:
-                
-                for i in reversed(range(ROWS)):
-                    if board[i][col] == 0:
-                        board[i][col] = RED
-                        pygame.draw.circle(SCREEN, RED, (SQUARE_SIZE*col+50, SQUARE_SIZE*i+50), 40)
-                        if check_winner(board,RED):
-                            winner_panel(SCREEN, RED)
-                            pygame.display.update()
-                            if event.type == pygame.MOUSEBUTTONDOWN:
-                                pos1 = pygame.mouse.get_pos()
-                                x1, y1 = pos1
+            if event.type == pygame.QUIT:
+                game_on = False
 
-                                #If you click outside the boxes
-                                if (55 > x1 > 550) or (400 > y1 > 475) or (350 < x1  < 450):
-                                    pass
-                                #Click the Play again button
-                                if (55 <= x1 <= 350) and (400 <= y1 <= 450):
-                                    board = empty_board()
-                                    draw(SCREEN)
-                                    turn = 0
-                                    break
-                                #Click the quit button
-                                elif (450 <= x1 <= 550) and (400 <= y1 <= 450):
-                                    game_on = False
-                            break
+            color = functions.player_turn(SCREEN,turn)
 
-                        turn += 1
+            if event.type == pygame.MOUSEBUTTONDOWN:    
+
+                pos = pygame.mouse.get_pos()
+                row, col = functions.get_row_col_from_mouse(pos)
+                x, y = pos
+
+                functions.place_piece(color, col, board, SCREEN, allowed)
+
+                if functions.check_winner(board, color):
+
+                    allowed = False     #now pieces won't be placed on the board while waiting on a decision
+                    functions.winner_panel(SCREEN, color)
+                    pygame.display.update()
+
+                    if functions.click_outside_boxes(x, y):
+                        pass
+
+                    if functions.click_play_again(x, y):                                              
+                        board = functions.initialize_board()
+                        functions.draw_board(SCREEN)
+                        turn = 0
+                        allowed = True
                         break
 
-            else:
-                pygame.draw.circle(SCREEN, BLACK, (300, 650), 30)
-                for i in reversed(range(ROWS)):
-                    if board[i][col] == 0:
-                        board[i][col] = BLACK
-                        pygame.draw.circle(SCREEN, BLACK, (SQUARE_SIZE*col+50, SQUARE_SIZE*i+50), 40)
-                        if check_winner(board, BLACK):
-                            winner_panel(SCREEN, BLACK)
-                            pygame.display.update()
-                            if event.type == pygame.MOUSEBUTTONDOWN:
-                                pos1 = pygame.mouse.get_pos()
-                                x1, y1 = pos1
-
-                                #If you click outside the boxes
-                                if (55 > x1 > 550) or (400 > y1 > 475) or (350 < x1  < 450):
-                                    pass
-                                #Click the Play again button
-                                if (55 <= x1 <= 350) and (400 <= y1 <= 450):
-                                    board = empty_board()
-                                    draw(SCREEN)
-                                    turn = 0
-                                    break
-                                #Click the quit button
-                                elif (450 <= x1 <= 550) and (400 <= y1 <= 450):
-                                    game_on = False
-                            break
-
-                        turn += 1
-                        break
-                
-    pygame.display.update()
+                    if functions.click_quit(x, y):                       
+                        game_on = False
             
-                       
-    
+                turn += 1
 
+        pygame.display.update()
+
+    pygame.quit()
 
     
+if __name__ == "__main__":
+    main() 
